04b28d17ffba422118bc3d63b647c607ab2c72dc
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-06-21 18:23:38 -0300

    Programa 1 e 2 realizados

---
+++ fundamentos/bloco-04-introducao-a-javascript/script.js
@@ -0,0 +1,15 @@
+let a = 1;
+let b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+let c = 10;
+let d = 78;
+
+if (c > d) {
+    console.log(c);
+}else { console.log(d);}
