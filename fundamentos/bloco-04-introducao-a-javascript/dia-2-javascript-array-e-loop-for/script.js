b9f0a2457439b8bbaadd2c3fe431e1788de9b643
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-06-22 19:28:45 -0300

    Resolução bonus final

--- fundamentos/bloco-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/script.js
@@ -1,9 +1,12 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
-let maior = numbers[0];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < numbers.length; index+=1) {
-    if (numbers[index]<maior) {
-        maior = numbers[index]
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (numbers[index] > numbers[secondIndex]) {
+        let position = numbers[index];
+        numbers[index] = numbers[secondIndex];
+        numbers[secondIndex] = position;
+      }
     }
-}
-console.log(maior);
+  }
+  console.log(numbers);
