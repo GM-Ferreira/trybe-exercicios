93e064a8339dd493117ccdc9e3bb291aae887322
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-06-22 18:50:20 -0300

    Resolução passo 6

--- fundamentos/bloco-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/script.js
@@ -1,12 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
 let soma = 0;
 
-for (let index = 0; index < numbers.length; index +=1) {
-    console.log (numbers[index]);
-    soma += numbers[index];   
-}
-// console.log(soma);
-if ((soma/numbers.length)>20) {
-    console.log('Valor maior que 20');
-}else{console.log('Valor menor ou igual a 20');}
-console.log(soma/numbers.length);
+for (let index = 0; index < numbers.length; index += 1) {
+    if( (numbers[index]%2)>0){
+        soma+=1;
+    }
+}console.log(soma);
