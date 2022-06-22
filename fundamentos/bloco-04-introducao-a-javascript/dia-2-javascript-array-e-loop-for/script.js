611350d6184acf8b7751f7e3396e65bf0341d8bb
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-06-22 18:58:44 -0300

    Resolução 9 e 8

--- fundamentos/bloco-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/script.js
@@ -1,8 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
 let soma = 0;
+let maior;
 
-for (let index = 0; index < numbers.length; index += 1) {
-    if( (numbers[index]%2)>0){
-        soma+=1;
-    }
-}console.log(soma);
+for (let index = 0; index < 26; index += 1){
+console.log(index/2);
+}
