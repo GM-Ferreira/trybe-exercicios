fd613bcde5e7d0850acfb7a3337364a696b6c240
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-06-23 18:37:11 -0300

    Exercicio 2

--- fundamentos/bloco-04-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/dia2.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/dia2.js
@@ -1,10 +1,20 @@
 // Exercicio 1 
 
-let fatorial = 10;
-let resultado = 1;
+// let fatorial = 10;
+// let resultado = 1;
 
-for(i = fatorial; i!== 0 ;i -=1){
-resultado *= i;
+// for(i = fatorial; i!== 0 ;i -=1){
+// resultado *= i;
+// }
+// console.log(resultado);
+
+// Exercicio 2
+
+let word = 'tryber';
+let invertido = [];
+
+for(index = 0; index < word.length; index +=1){
+invertido.unshift(word[index]);
+
 }
-console.log(resultado);
-
+console.log(invertido);
