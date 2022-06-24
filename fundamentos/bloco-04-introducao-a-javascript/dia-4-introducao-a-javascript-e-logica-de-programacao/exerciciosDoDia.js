2e92e46b28077a381e16fda11d07fd269adc88dc
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-06-24 19:19:35 -0300

    Finalizado

--- fundamentos/bloco-04-introducao-a-javascript/dia-4-introducao-a-javascript-e-logica-de-programacao/exerciciosDoDia.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-4-introducao-a-javascript-e-logica-de-programacao/exerciciosDoDia.js
@@ -1,14 +1,14 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
 //   console.log('Bem vinda, ', info.personagem);
 
 // Parte 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
 
-info.recorrente = 'Sim';
+// info.recorrente = 'Sim';
 //outra opção -> info['recorrente'] = 'Sim';
 
 // console.log(info)
@@ -27,18 +27,47 @@ info.recorrente = 'Sim';
 
 // Parte 5
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim',
+//   };
+
+// for(nick in info){
+//     if(info[nick] !== info2[nick]){
+//         console.log(info[nick]+' e '+info2[nick])
+//     }else{
+//         console.log('Ambos '+nick+'s')
+//     }
+// }
+// console.log(info.personagem = info.personagem+' e '+'novo');
+
+//////////////////////////////////////////////////////////////////
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
 
-for(nick in info){
-    if(info[nick] !== info2[nick]){
-        console.log(info[nick]+' e '+info2[nick])
-    }else{
-        console.log('Ambos '+nick+'s')
-    }
-}
-// console.log(info.personagem = info.personagem+' e '+'novo');
+//   console.log('O livro favorito de '+leitor.nome+' '+leitor.sobrenome+' se chama '+"'"+ leitor.livrosFavoritos[0].titulo);
+
+ leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  })
+
+  console.log(leitor.livrosFavoritos);
+
+  // Parte 8
+  
+  console.log('Julia tem '+leitor.livrosFavoritos.length+" livros favoritos");
