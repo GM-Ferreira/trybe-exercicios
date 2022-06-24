5fb629bb43d1b6be968c32d5a6fb98c9bd225f3d
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-06-24 15:55:27 -0300

    Refazendo exercicios com funções

--- fundamentos/bloco-04-introducao-a-javascript/exercicio01.js
+++ fundamentos/bloco-04-introducao-a-javascript/exercicio01.js
@@ -24,14 +24,67 @@
 //     console.log('Olá ', names[index]);
 //   }
 
-  // Parte 3
+// Parte 3
 
-  let car = {
-    model: 'A3 Sedan',
-    manufacturer: 'Audi',
-    year: 2020
-  };
+//   let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
 
-  for( let index in car){
-    console.log(index,': ', car[index]);
-  }
+//   for( let index in car){
+//     console.log(index,': ', car[index]);
+//   }
+
+// Parte 1 refazendo
+// function operacoesMatematicas (a, b){
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// }
+// operacoesMatematicas(1 , 2)
+
+// Parte 2 e 3 refazendo
+
+// function maiorValor (c, d, e){
+
+//     if (c > d && c > e) {
+//     console.log(c);
+// } else if (d > c && d > e) { console.log(d); }
+// else { console.log(e); }
+// }
+
+// maiorValor (15, 25, 10)
+
+// Parte 4 refazendo
+
+// function identificadorDeNumero (f){
+
+// if (f > 0) {
+//     console.log('Positive');
+// }else if (f < 0) {
+//     console.log('Negative');
+// }else {
+//     console.log('Zero');
+// }
+// }
+
+// identificadorDeNumero (10)
+
+// Parte 5 refazendo
+
+function ehUmTriangulo (t1, t2, t3){
+
+if (t1>0 && t2>0 && t3>0 && (t1+t2+t3)>180) {
+    console.log(false);
+}else if(t1>0 && t2>0 && t3>0 && (t1+t2+t3)<=180){
+    console.log(true);
+}else {
+    console.log('Valores invalidos');
+}
+}
+
+ehUmTriangulo( 10, 25, 80)
