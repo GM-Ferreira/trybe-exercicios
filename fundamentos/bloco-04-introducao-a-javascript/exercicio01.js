2ea650a74cb7bdd3bd426e0a095ac7336a7d5be4
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-06-24 15:17:06 -0300

    Exercicios de fixação

---
+++ fundamentos/bloco-04-introducao-a-javascript/exercicio01.js
@@ -0,0 +1,37 @@
+// Parte 1
+
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: { golden: 2, silver: 3 },
+//     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+// }
+
+// console.log('O player se chama ' + player.name + ' ' + player.lastName + ' e tem ' + player.age + ' anos de idade.');
+
+// console.log(player.bestInTheWorld);
+
+// Parte 2
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+
+//   for( let index in names){
+//     console.log('Olá ', names[index]);
+//   }
+
+  // Parte 3
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for( let index in car){
+    console.log(index,': ', car[index]);
+  }
