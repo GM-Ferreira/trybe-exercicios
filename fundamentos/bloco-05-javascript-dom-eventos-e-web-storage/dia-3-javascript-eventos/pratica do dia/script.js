533dc441981c0a9d47b70b360b082a9043e3ca8f
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-06-30 18:51:04 -0300

    Exercicio 1

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/pratica do dia/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/pratica do dia/script.js
@@ -1,16 +1,34 @@
 function createDaysOfTheWeek() {
     const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
-  
+
     for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
+        const days = weekDays[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = days;
+
+        weekDaysList.appendChild(dayListItem);
     };
-  };
-  
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
+};
+
+createDaysOfTheWeek();
+
+const listaUl = document.getElementById('days');
+
+let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+    for (let index = 0; index < decemberDaysList.length; index += 1) {
+        const newDay = document.createElement('li');
+        newDay.innerHTML = decemberDaysList[index];
+        newDay.className = 'day';
+        
+        if(decemberDaysList[index] === 24 || decemberDaysList[index] === 25 || decemberDaysList[index] === 31){
+            newDay.classList.add('holiday');
+        }
+
+        if(decemberDaysList[index] === 4 || decemberDaysList[index] === 11 || decemberDaysList[index] === 18 || decemberDaysList[index] === 25){
+            newDay.classList.add('friday');
+        }
+
+        listaUl.appendChild(newDay);
+    }
