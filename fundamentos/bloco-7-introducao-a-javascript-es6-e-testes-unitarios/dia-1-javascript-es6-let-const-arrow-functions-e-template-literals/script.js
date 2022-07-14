e2f200d2a28dd44c542fe1277f700a2818c204b2
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-07-14 18:36:52 -0300

    Exercicios parte I

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,22 @@
+///////// Parte 1 //////////////
+
+// const testingScope = (escopo) => {
+//     if (escopo === false) {
+//         let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//         ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//         console.log(ifScope);
+//     } else {
+//         const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//         console.log(elseScope);
+//     }
+// }
+
+// testingScope(true);
+
+///////// Parte 2 ///////////
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort(function(a, b){return a - b});
+
+console.log(`Os números ${oddsAndEvens} se encontram em ordem crescente!`); // será necessário alterar essa linha 😉
