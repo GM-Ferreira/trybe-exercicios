2f4a679e63841384860ad007f03f60a15f5e1964
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-07-14 21:46:22 -0300

    Exercicio 3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,3 +1,6 @@
+const contador = document.getElementById('contador');
+const botao = document.getElementById('clickbtn');
+
 ///////// Parte 1 //////////////
 
 // const testingScope = (escopo) => {
@@ -13,10 +16,46 @@
 
 // testingScope(true);
 
-///////// Parte 2 ///////////
+///////// Parte 1.2 ///////////
 
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-oddsAndEvens.sort(function(a, b){return a - b});
+// oddsAndEvens.sort(function(a, b){return a - b});
 
-console.log(`Os números ${oddsAndEvens} se encontram em ordem crescente!`); // será necessário alterar essa linha 😉
+// console.log(`Os números ${oddsAndEvens} se encontram em ordem crescente!`); // será necessário alterar essa linha 😉
+
+///// Parte 2 ////////// Retorno o Fatorial
+
+// const fatorial = (numero) => {
+//     let contador = 1;
+//     for (let index = 1; index <= numero; index += 1) {
+//         contador *= index;
+//     }
+//     console.log(`O valor do fatorial de ${numero} é ${contador}`);
+// }
+
+// fatorial(6);
+
+///// Parte 2.2 ////////// maior palavra
+
+// const encontreMaior = (longestWord) => {
+//     const separado = longestWord.split(' ');
+//     let palavra = separado[0];
+//     for (let i = 0; i < separado.length; i += 1) {
+//         if (palavra.length < separado[i].length) {
+//             palavra = separado[i];
+//         };
+//     }
+//     console.log(palavra);
+// }
+
+// encontreMaior('Antônio foi no banheiro e não sabemos o que aconteceu');
+
+////// Parte 2.3 ////// contador de clicks
+let numero = 0;
+
+botao.addEventListener('click', acionado = () => {  
+    numero += 1;
+    contador.innerText = `Quantidade de clicks: ${numero}`
+    console.log(' ');
+})
