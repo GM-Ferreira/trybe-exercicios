83a297ff5bb60c3881c4c3b37cff05b3a9f64585
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-07-25 14:49:25 -0300

    Atualizações

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-higher-order-functions-map-e-filter/pratica00.js
@@ -0,0 +1,8 @@
+const arrayMyStudents = ['Maria Eduarda', 'Manuela Larissa', 'Jorge Paulo', 'Ricardo Moraes', 'Wilson'];
+
+const removeStudentByName = (name, listStudents) =>
+  listStudents.filter((student) => student !== name);
+  // Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
+
+const newListStudents = removeStudentByName('Ricardo', arrayMyStudents);
+console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
